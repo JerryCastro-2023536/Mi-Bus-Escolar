79a754da51d6c5dbb7b4ad783de4a5b3bad3071d
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-08-27 21:54:23 -0600

    feat: se ha agregado el model del servicios

---
+++ backend/src/models/Servicios.ts
@@ -0,0 +1,11 @@
+import {Estados} from "./EstadosActivosInactivos"
+
+export interface Servicios{
+    id_servicio: number,
+    id_proveedor: number,
+    nombre: string,
+    descripcion: string,
+    precio_mensual: number,
+    estado: Estados,
+    fecha_creacion: Date
+}
