deee00d1fda7ef02731fbfb8dcbbd11b0c1bdcdc
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-08-28 21:21:36 -0600

    feat: Se agrego el modelo para la entidad Ruta_parada

---
+++ backend/src/models/rutaParada.ts
@@ -0,0 +1,8 @@
+export interface rutaParada{
+    id_ruta_parada: number;
+    id_ruta: number;
+    id_parada: number;
+    orden_parada: number;
+    minutos_estimados: number
+    hora_estimada: Date
+}
