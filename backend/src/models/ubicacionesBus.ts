3c3964ab4773b91a26e98a32b121dc92349d407e
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-08-29 23:55:31 -0600

    fix: Se corrigio modelado

--- backend/src/models/ubicacionesBus.ts
+++ backend/src/models/ubicacionesBus.ts
@@ -2,6 +2,7 @@ export interface UbicacionesBus{
     id_ubicacion?: number;
     id_viaje: number;
     latitud: number;
+    longitud: number;
     velocidad: number | null;
     fecha_hora: Date;
 }
