1a27965c187b6672fb6ee6de4e3b6d3b07fd728c
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-08-30 00:22:49 -0600

    feat: Se agrego el router de paradas

---
+++ backend/src/routes/paradas.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { deleteParada, getParadaById, getParadas, postParadas, putParada } from "../controllers/paradas.controller";
+
+const router = Router();
+
+router.get("/paradas", getParadas);
+router.post("/paradas", postParadas);
+router.get("/paradas/:id", getParadaById);
+router.put("/paradas/:id", putParada);
+router.delete("/paradas/:id", deleteParada)
