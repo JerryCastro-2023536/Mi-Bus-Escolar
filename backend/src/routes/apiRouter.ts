86bdb5b06e36f6c64d36b80dc6a80a1d90a31da4
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-08-29 23:59:24 -0600

    feat: Se agrego la ruta ubicaciones bus al router general

--- backend/src/routes/apiRouter.ts
+++ backend/src/routes/apiRouter.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usuarioRoutes from './usuario.route';
 import viajesRoutes from './viaje.route'
+import  ubicacionesRoutes  from "./ubicacionesBus.route";
 
 const apiRouter = Router();
 apiRouter.use(usuarioRoutes);
 apiRouter.use(viajesRoutes);
+apiRouter.use(ubicacionesRoutes);
 
 export default apiRouter;
