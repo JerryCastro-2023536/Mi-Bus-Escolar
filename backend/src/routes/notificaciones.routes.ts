2aadb6409fe1b27270ea87434562331d95f6b742
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-08-30 00:22:33 -0600

    feat: Se agrego el router de notificaciones

---
+++ backend/src/routes/notificaciones.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { deleteNotificacion, getNotificacionById, getNotificaciones, postNotificaciones, putNotificacion } from "../controllers/notificaciones.controller";
+
+const router = Router();
+
+router.get("/notificaciones", getNotificaciones);
+router.get("/notificaciones/:id", getNotificacionById);
+router.post("/notificaciones", postNotificaciones);
+router.put("/notificaciones/:id", putNotificacion);
+router.delete("/notificaciones/:id", deleteNotificacion);
