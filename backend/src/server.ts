83c83bd12e956d320a1640bfca2c956d8858d783
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-08-29 22:29:35 -0600

    feat: Se agrego el server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,20 @@
+import  dotenv  from 'dotenv';
+import cors from 'cors';
 import { pruebaConexion } from "./config/conexion";
+import express from 'express';
+import apiRouter from './routes/apiRouter';
 
+dotenv.config();
+const app = express();
+const PORT = process.env.APP_PORT;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 pruebaConexion();
+
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+    console.log(`SERVIDOR EJECUTANDOSE EN PUERTO: ${PORT}` )
+});
