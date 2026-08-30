7b9e93ef7a2d4ce333e824a9ebc940f1092be666
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-08-29 23:42:03 -0600

    feat: Se agrego servicio para viajes

---
+++ backend/src/services/viajes.services.ts
@@ -0,0 +1,31 @@
+import { pool } from "../config/conexion";
+import { Viajes } from "../models/viajes";
+
+export async function listarViajes() {
+    const resultado = await pool.query("SELECT * FROM viajes");
+    return resultado.rows;
+}
+
+export async function buscarViajeById(id: number) {
+    const res = await pool.query('SELECT * FROM viajes WHERE id_viaje = $1', [id]);
+    return res.rows[0] || null;
+}
+
+export async function agregarViaje(v: Viajes) {
+    const values = [v.id_ruta, v.id_chofer, v.id_vehiculo, v.fecha_viaje, v.hora_inicio, v.hora_fin, v.estado]
+    const query = 'INSERT INTO viajes(id_ruta, id_chofer, id_vehiculo, fecha_viaje, hora_inicio, hora_fin, estado) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *'
+    const res = await pool.query(query, values);
+    return res.rows[0];
+}
+
+export async function editarViajeById(id: number, v: Viajes) {
+    const values = [v.id_ruta, v.id_chofer, v.id_vehiculo, v.fecha_viaje, v.hora_inicio, v.hora_fin, v.estado, id]
+    const query = 'UPDATE viajes SET id_ruta = $1, id_chofer = $2, id_vehiculo = $3, fecha_viaje = $4, hora_inicio = $5, hora_fin = $6, estado = $7 WHERE id_viaje = $8 RETURNING *';
+    const res = await pool.query(query, values);
+    return res.rows[0] || null;
+}
+
+export async function eliminarViajeById(id: number) {
+    const res = await pool.query('DELETE FROM viajes WHERE id_viaje = $1', [id])
+    return (res.rowCount ?? 0) > 0
+}
