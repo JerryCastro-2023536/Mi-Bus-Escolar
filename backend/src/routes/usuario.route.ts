e700051638d7607172e059200148916ae5caf678
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-08-29 22:29:28 -0600

    feat: Se agregaron las rutas de usuario

---
+++ backend/src/routes/usuario.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getUsuarios, getUsuarioById, postUsuario, putUsuarioByID, deleteUsuarioById } from "../controllers/usuario.controllers";
+const router = Router();
+
+router.get("/usuarios", getUsuarios);
+router.get("/usuarios/:id", getUsuarioById);
+router.post("/usuarios", postUsuario);
+router.put("/usuarios/:id", putUsuarioByID);
+router.delete("/usuarios/:id", deleteUsuarioById);
+
+export default router;
