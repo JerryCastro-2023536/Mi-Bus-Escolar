b5742269b0fc714f6e9b92f711b004fcdbe7b3e6
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-02 11:36:36 -0600

    refactor: se ha corregido las querys de incidencias.service

--- backend/src/services/incidencias.service.ts
+++ backend/src/services/incidencias.service.ts
@@ -15,7 +15,7 @@ export async function listarIncidencias(){
 
 export async function agregarIncidencia(inc: Incidencias){
     try{
-        const consulta = "insert into incidencias(id_viaje, id_ruta, id_usuario_reporta, titulo, descripcion, latitud, longitud, fecha_hora, estado) values($1, $2, $3, $4, $5, $6, $7, $8, $9)"
+        const consulta = "insert into incidencias(id_viaje, id_ruta, id_usuario_reporta, titulo, descripcion, latitud, longitud, fecha_hora, estado) values($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *"
         const values = [inc.id_viaje, inc.id_ruta, inc.id_usuario_reporta, inc.titulo, inc.descripcion, inc.latitud, inc.longitud, inc.fecha_hora, inc.estado]
         const resultado = await pool.query(consulta, values)
         return resultado.rows[0]
@@ -41,7 +41,7 @@ export async function buscarIncidencia(id: number){
 export async function actualizarIncidencia(id: number, inc: Incidencias){
     try{
         const values = [inc.id_viaje, inc.id_ruta, inc.id_usuario_reporta, inc.titulo, inc.descripcion, inc.latitud, inc.longitud, inc.fecha_hora, inc.estado, id]
-        const consulta = "update incidencias set id_viaje=$1, id_ruta=$2, id_usuario_reporta=$3, titulo=$4, descripcion=$5, latitud=$6, longitud=$7, fecha_hora=$8, estado=$9 where id_incidencia=$10"
+        const consulta = "update incidencias set id_viaje=$1, id_ruta=$2, id_usuario_reporta=$3, titulo=$4, descripcion=$5, latitud=$6, longitud=$7, fecha_hora=$8, estado=$9 where id_incidencia=$10 returning *"
         const resultado = await pool.query(consulta, values)
         
         if(!resultado.rows[0]){
