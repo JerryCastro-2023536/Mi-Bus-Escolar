2da0d575300d2bce211be3133351fc8f1c13bc00
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-02 08:33:27 -0600

    feat: Se agrego notFoundError al errorHandler

--- backend/src/errors/errorHandler.ts
+++ backend/src/errors/errorHandler.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { ValidationError } from './validation.error';
 import { DatabaseError } from './database.error';
 import { InternalError } from './500.error';
+import { NotFoundError } from './notFound.error';
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     console.error(`[Error]: ${err.message}`); 
@@ -19,6 +20,13 @@ export function errorHandler(err: Error, req: Request, res: Response, next: Next
             success: false,
             message: err.message,
             error: err.error
+        })
+    }
+
+    if (err instanceof NotFoundError){
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message
         })
     }
 
