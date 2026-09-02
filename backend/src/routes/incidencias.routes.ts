7a7dbe942c44c9941cf7acebcbacb488d55508f0
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-01 20:32:50 -0600

    refactor: se han agregado las importaciones del validateSchema y tambien se llamaron las validaciones de incidencias

--- backend/src/routes/incidencias.routes.ts
+++ backend/src/routes/incidencias.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import { obtenerIncidencias, obtenerIncidenciaPorId, crearIncidencia, editarIncidencia, eliminarIncidencias } from "../controllers/incidencias.controller"
-
+import { validateSchema } from "../utils/middleware/schemaValidator";
+import { createIncidenciaSchema, updateIncidenciaSchema } from "../validators/incidencias.validator";
 const router = Router();
 
 router.get("/incidencias", obtenerIncidencias);
 router.get("/incidencia/:id", obtenerIncidenciaPorId);
-router.post("/incidencias", crearIncidencia);
-router.put("/incidencias/:id", editarIncidencia);
+router.post("/incidencias", validateSchema(createIncidenciaSchema), crearIncidencia);
+router.put("/incidencias/:id",validateSchema(updateIncidenciaSchema) ,editarIncidencia);
 router.delete("/incidencias/:id", eliminarIncidencias);
 
 export default router;
