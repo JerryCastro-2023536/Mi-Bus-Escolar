6d4b286e7c95dc068c937ba1b43399ecc5eaf0e9
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-01 20:59:45 -0600

    feat: se agregaroin nuevas excepciones controladas en el error handler

--- backend/src/errors/errorHandler.ts
+++ backend/src/errors/errorHandler.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { ValidationError } from './validation.error';
+import { DatabaseError } from './database.error';
+import { InternalError } from './500.error';
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     console.error(`[Error]: ${err.message}`); 
@@ -12,8 +14,18 @@ export function errorHandler(err: Error, req: Request, res: Response, next: Next
         });
     }
 
-    return res.status(500).json({
-        success: false,
-        message: "Ocurrió un error interno en el servidor"
-    });
+   if (err instanceof DatabaseError){
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            error: err.error
+        })
+    }
+
+    if (err instanceof InternalError){
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message
+        })
+    }
 }
