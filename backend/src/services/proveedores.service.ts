62b6bc5fdd155172d5890244012fd0aaed1ed0a4
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-02 11:37:42 -0600

    refactor: se ha agregado el trycatch para manejo de errores en proveedores.service

--- backend/src/services/proveedores.service.ts
+++ backend/src/services/proveedores.service.ts
@@ -1,32 +1,67 @@
 import { pool } from "../config/conexion";
+import { NotFoundError } from "../errors/notFound.error";
 import { Proveedores } from "../models/Proveedores";
+import { errorThrower } from "../utils/middleware/errorThrower";
 
 export async function listarProveedores(){
-    const consulta = await pool.query("select * from proveedores");
-    return consulta.rows;
+    try{
+        const consulta = await pool.query("select * from proveedores");
+        return consulta.rows;
+    }catch(error){
+        errorThrower(error)
+    }
 }
 
 export async function agregarProveedor(prov: Proveedores){
-    const values = [prov.id_usuario, prov.nombre_negocio, prov.direccion, prov.telefono_contacto]
-    const consulta = "insert into proveedores(id_usuario, nombre_negocio, direccion, telefono_contacto) values($1, $2, $3, $4)"
-    const resultado = await pool.query(consulta, values)
-    return resultado.rows[0];
+    try{
+        const values = [prov.id_usuario, prov.nombre_negocio, prov.direccion, prov.telefono_contacto]
+        const consulta = "insert into proveedores(id_usuario, nombre_negocio, direccion, telefono_contacto) values($1, $2, $3, $4) returning *"
+        const resultado = await pool.query(consulta, values)
+        return resultado.rows[0];
+    }catch(error){
+        errorThrower(error)
+    }
 }
 
 export async function buscarProveedor(id: number){
-    const resultado = await pool.query("select * from proveedores where id_proveedor = $1", [id])
-    return resultado.rows[0]
+    try{
+        const resultado = await pool.query("select * from proveedores where id_proveedor = $1", [id])
+
+        if(!resultado.rows[0]){
+            throw new NotFoundError(`el id del proveedor ${id} no se encontro`)
+        }
+        return resultado.rows[0]
+    }catch(error){
+        errorThrower(error)
+    }
 }
 
 export async function actualizarProveedor(id: number, prov: Proveedores){
-    const values = [prov.id_usuario, prov.nombre_negocio, prov.direccion, prov.telefono_contacto, id]
-    const consulta = "update proveedores set id_usuario=$1, nombre_negocio=$2, direccion=$3, telefono_contacto=$4 where id_proveedor=$5"
-    const resultado = await pool.query(consulta, values)
-    return resultado.rows[0];
+    try{
+        const values = [prov.id_usuario, prov.nombre_negocio, prov.direccion, prov.telefono_contacto, id]
+        const consulta = "update proveedores set id_usuario=$1, nombre_negocio=$2, direccion=$3, telefono_contacto=$4 where id_proveedor=$5 returning *"
+        const resultado = await pool.query(consulta, values)
+
+        if(!resultado.rows[0]){
+            throw new NotFoundError(`no se pudo editar el proveedor porque el id ${id} no existe`)
+        }
+
+        return resultado.rows[0];
+    }catch(error){
+        errorThrower(error)
+    }
 }
 
 export async function eliminarProveedor(id: number){
-    const consulta = await pool.query("delete from proveedores where id_proveedor = $1", [id]);
-    console.log("se ha eliminado el proveedor correctamente")
-    return (consulta.rowCount ?? 0) > 0
+    try{
+        const consulta = await pool.query("delete from proveedores where id_proveedor = $1", [id]);
+        
+        if(consulta.rowCount === 0){
+            throw new NotFoundError("no se pudo eliminar el proveedor porque el id no existe")
+        }
+
+        return true
+    }catch(error){
+        errorThrower(error)
+    }
 }
