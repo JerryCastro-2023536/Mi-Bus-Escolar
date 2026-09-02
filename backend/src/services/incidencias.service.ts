5266558d30f156e98cb81cfada5025053c5d19c8
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-02 10:38:44 -0600

    refactor: se agreron los try catch en el service de incidencias para el manejo de errores

--- backend/src/services/incidencias.service.ts
+++ backend/src/services/incidencias.service.ts
@@ -1,32 +1,69 @@
+import e from "cors";
 import { pool } from "../config/conexion";
 import { Incidencias } from "../models/Incidencias";
+import { errorThrower } from "../utils/middleware/errorThrower";
+import { NotFoundError } from "../errors/notFound.error";
 
 export async function listarIncidencias(){
-    const consulta = await pool.query("select * from incidencias");
-    return consulta.rows;
+    try{
+        const consulta = await pool.query("select * from incidencias");
+        return consulta.rows;
+    }catch(error){
+        errorThrower(error)
+    }
 }
 
 export async function agregarIncidencia(inc: Incidencias){
-    const values = [inc.id_viaje, inc.id_ruta, inc.id_usuario_reporta, inc.titulo, inc.descripcion, inc.latitud, inc.longitud, inc.fecha_hora, inc.estado]
-    const consulta = "insert into incidencias(id_viaje, id_ruta, id_usuario_reporta, titulo, descripcion, latitud, longitud, fecha_hora, estado) values($1, $2, $3, $4, $5, $6, $7, $8, $9)"
-    const resultado = await pool.query(consulta, values)
-    return resultado.rows[0];
+    try{
+        const consulta = "insert into incidencias(id_viaje, id_ruta, id_usuario_reporta, titulo, descripcion, latitud, longitud, fecha_hora, estado) values($1, $2, $3, $4, $5, $6, $7, $8, $9)"
+        const values = [inc.id_viaje, inc.id_ruta, inc.id_usuario_reporta, inc.titulo, inc.descripcion, inc.latitud, inc.longitud, inc.fecha_hora, inc.estado]
+        const resultado = await pool.query(consulta, values)
+        return resultado.rows[0]
+    }catch(error){
+        errorThrower(error)
+    }
 }
 
 export async function buscarIncidencia(id: number){
-    const resultado = await pool.query("select * from incidencias where id_incidencia = $1", [id])
-    return resultado.rows[0]
+    try{
+        const resultado = await pool.query("select * from incidencias where id_incidencia = $1", [id])
+        if(!resultado.rows[0]){
+            throw new NotFoundError(`la incidencia con el id ${id} no se encontro`)
+        }
+
+        return resultado.rows[0]
+    }catch(error){
+        errorThrower(error)
+    }
+    
 }
 
 export async function actualizarIncidencia(id: number, inc: Incidencias){
-    const values = [inc.id_viaje, inc.id_ruta, inc.id_usuario_reporta, inc.titulo, inc.descripcion, inc.latitud, inc.longitud, inc.fecha_hora, inc.estado, id]
-    const consulta = "update incidencias set id_viaje=$1, id_ruta=$2, id_usuario_reporta=$3, titulo=$4, descripcion=$5, latitud=$6, longitud=$7, fecha_hora=$8, estado=$9 where id_incidencia=$10"
-    const resultado = await pool.query(consulta, values)
-    return resultado.rows[0];
+    try{
+        const values = [inc.id_viaje, inc.id_ruta, inc.id_usuario_reporta, inc.titulo, inc.descripcion, inc.latitud, inc.longitud, inc.fecha_hora, inc.estado, id]
+        const consulta = "update incidencias set id_viaje=$1, id_ruta=$2, id_usuario_reporta=$3, titulo=$4, descripcion=$5, latitud=$6, longitud=$7, fecha_hora=$8, estado=$9 where id_incidencia=$10"
+        const resultado = await pool.query(consulta, values)
+        
+        if(!resultado.rows[0]){
+            throw new NotFoundError("no se pudo editar ya que la incidencia porque el id no existe")
+        }
+
+        return resultado.rows[0]
+    }catch(error){
+        errorThrower(error)
+    }
+
 }
 
 export async function eliminarIncidencia(id: number){
-    const consulta = await pool.query("delete from incidencias where id_incidencia = $1", [id]);
-    console.log("se ha eliminado la incidencia correctamente")
-    return (consulta.rowCount ?? 0 ) >0
+    try{
+        const consulta = await pool.query("delete from incidencias where id_incidencia = $1", [id]);
+        if(consulta.rowCount === 0){
+            throw new NotFoundError("no se pudo eliminar la incidencia porque el id no existe")
+        }
+
+        return true
+    }catch(error){
+        errorThrower(error)
+    }
 }
