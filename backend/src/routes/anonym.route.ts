5b4521f7cd0689d4e2306e08a6627bd892af546b
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-02 23:18:11 -0600

    feat: Se añadio esquema de validacion de login a la ruta

--- backend/src/routes/anonym.route.ts
+++ backend/src/routes/anonym.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { loginUsuario } from '../controllers/usuario.controller';
+import { validateSchema } from '../utils/middleware/schemaValidator';
+import { loginUserSchema } from '../validators/user.validator';
 
 const router = Router();
 
-router.post('/login', loginUsuario);
+router.post('/login', validateSchema(loginUserSchema),loginUsuario);
 
 export default router;
