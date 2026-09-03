51169a876c3f7fa8630625299368cfb2330f8dcb
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-09-02 18:13:58 -0600

    feat: Se agregaron las validaciones de pagos

--- backend/src/routes/pagos.routes.ts
+++ backend/src/routes/pagos.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { deletePago, getPagoById, getPagos, postPagos, putPago } from "../controllers/pagos.controller";
-import zod from "zod"
+import { validateSchema } from "../utils/middleware/schemaValidator";
+import { createPagoSchema, updatePagoSchema } from "../validators/pagos.validator";
 
 
 const router = Router();
 
 router.get("/pagos", getPagos);
 router.get("/pagos/:id", getPagoById);
-router.post("/pagos", postPagos);
-router.put("/pagos/:id", putPago);
+router.post("/pagos", validateSchema(createPagoSchema), postPagos);
+router.put("/pagos/:id", validateSchema(updatePagoSchema), putPago);
 router.delete("/pagos/:id", deletePago);
 
 export default router;
