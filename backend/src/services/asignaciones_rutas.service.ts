2426b6a05ecc962313c85096caf6db8a76546622
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-09-02 19:09:58 -0600

    feat: Se agregaron las validaciones de las consultas de asignaciones

--- backend/src/services/asignaciones_rutas.service.ts
+++ backend/src/services/asignaciones_rutas.service.ts
@@ -1,33 +1,64 @@
 import { pool } from "../config/Conexion";
+import { NotFoundError } from "../errors/notFound.error";
 import { Asignaciones_Ruta } from "../models/Asignaciones_Rutas";
+import { errorThrower } from "../utils/middleware/errorThrower";
 
 export async function listarAsignacionesRutas() {
-    const resultado = await pool.query("SELECT * FROM asignaciones_ruta");
-    return resultado.rows;
+    try {
+        const resultado = await pool.query("SELECT * FROM asignaciones_ruta");
+        return resultado.rows;
+    } catch (error) {
+        errorThrower(error);
+    }
 }
 
 export async function agregarAsignacionesRutas(p_asignacion_ruta : Asignaciones_Ruta){
-    const valores = [p_asignacion_ruta.id_estudiante, p_asignacion_ruta.id_ruta, p_asignacion_ruta.id_parada_recogida, p_asignacion_ruta.id_parada_descenso];
-    const consulta = `INSERT INTO asignaciones_ruta(id_estudiante, id_ruta, id_parada_recogida, id_parada_descenso) VALUES ($1, $2, $3, $4)`
-    const resultado = await pool.query(consulta, valores);
-    return resultado.rows[0];
+    try {
+        const valores = [p_asignacion_ruta.id_estudiante, p_asignacion_ruta.id_ruta, p_asignacion_ruta.id_parada_recogida, p_asignacion_ruta.id_parada_descenso];
+        const consulta = `INSERT INTO asignaciones_ruta(id_estudiante, id_ruta, id_parada_recogida, id_parada_descenso) VALUES ($1, $2, $3, $4)`
+        const resultado = await pool.query(consulta, valores);
+        return resultado.rows[0];
+    } catch (error) {
+        errorThrower(error);
+    }
 }
 
 export async function buscarAsignacionRutaPorId(id : number){
-    const valor = [id];
-    const resultado = await pool.query("SELECT * FROM asignaciones_ruta WHERE id_asignacion = $1", valor);
-    return resultado.rows[0];
+    try {
+        const valor = [id];
+        const resultado = await pool.query("SELECT * FROM asignaciones_ruta WHERE id_asignacion = $1", valor);
+        if (!resultado.rows[0]) {
+            throw new NotFoundError(`La asignación con ID ${id} no fue encontrada.`);
+        }
+        return resultado.rows[0];
+    } catch (error) {
+        errorThrower(error);
+    }
 }
 
 export async function actualizarAsignacionRuta(p_asignacion_ruta : Asignaciones_Ruta, id: number){
-    const valores = [p_asignacion_ruta.id_estudiante, p_asignacion_ruta.id_ruta, p_asignacion_ruta.id_parada_recogida, p_asignacion_ruta.id_parada_descenso, id];
-    const consulta = `UPDATE asignaciones_ruta SET id_estudiante = $1, id_ruta = $2, id_parada_recogida = $3, id_parada_descenso = $4 WHERE id_asignacion = $5`;
-    const resultado = await pool.query(consulta, valores);
-    return resultado.rows[0];
+    try {
+        const valores = [p_asignacion_ruta.id_estudiante, p_asignacion_ruta.id_ruta, p_asignacion_ruta.id_parada_recogida, p_asignacion_ruta.id_parada_descenso, id];
+        const consulta = `UPDATE asignaciones_ruta SET id_estudiante = $1, id_ruta = $2, id_parada_recogida = $3, id_parada_descenso = $4 WHERE id_asignacion = $5`;
+        const resultado = await pool.query(consulta, valores);
+        if (!resultado.rows[0]) {
+            throw new NotFoundError(`La asignación con ID ${id} no fue encontrada.`);
+        }
+        return resultado.rows[0];
+    } catch (error) {
+        errorThrower(error);
+    }
 }
 
 export async function eliminarAsignacionRuta(id : number) {
-    const valor = [id];
-    const resultado = await pool.query("DELETE FROM asignaciones_ruta WHERE id_asignacion = $1", valor);
-    console.log("Eliminado correctamente");
+    try {
+        const valor = [id];
+        const resultado = await pool.query("DELETE FROM asignaciones_ruta WHERE id_asignacion = $1", valor);
+        if (resultado.rowCount === 0) {
+            throw new NotFoundError(`La asignación con ID ${id} no fue encontrada.`);
+        }
+        return true;
+    } catch (error) {
+        errorThrower(error);
+    }
 }
