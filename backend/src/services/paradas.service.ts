205ba9a9369fc93e241bdbd107eddab08930f2fb
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-05 23:33:23 -0600

    refactor: Se reemplazaron las query quemadas por procedimientos almacenados en la entidad Paradas

--- backend/src/services/paradas.service.ts
+++ backend/src/services/paradas.service.ts
@@ -1,71 +1,68 @@
-import { pool } from "../config/Conexion";
+import { pool } from "../config/conexion";
 import { NotFoundError } from "../errors/notFound.error";
 import { Paradas } from "../models/Paradas";
 import { errorThrower } from "../utils/middleware/errorThrower";
 
 export async function listarParadas() {
-    try{
-        const resultado = await pool.query("SELECT * FROM paradas");
+    try {
+        const resultado = await pool.query("SELECT * FROM sp_paradas_listar()");
         return resultado.rows;
-    }catch(error){
-        errorThrower(error);
-    }
-    
-}
-
-export async function agregarParadas(p_paradas : Paradas){
-    try{
-        const valores = [p_paradas.nombre, p_paradas.direccion, p_paradas.latitud, p_paradas.longitud];
-        const consulta = `INSERT INTO paradas(nombre, direccion, latitud, longitud) VALUES ($1, $2, $3, $4)`
-        const resultado = await pool.query(consulta, valores);
-        return resultado.rows[0];
-    }catch(error){
+    } catch (error) {
         errorThrower(error);
     }
 }
 
-export async function buscarParadaPorId(id : number){
-    try{
-        const valor = [id];
-        const resultado = await pool.query("SELECT * FROM paradas WHERE id_parada = $1", valor);
+export async function buscarParadaById(id: number) {
+    try {
+        const res = await pool.query("SELECT * FROM sp_paradas_buscar_por_id($1)", [id]);
 
-        if(!resultado.rows[0]){
-            throw new NotFoundError(`La parada con ID ${id} no fue encontrado.`)
+        if (!res.rows[0]) {
+            throw new NotFoundError(`La parada con ID ${id} no fue encontrada.`);
         }
-        return resultado.rows[0];
-    }catch(error){
-        errorThrower(error)
-    }
-}
 
-export async function actualizarParada(p_paradas : Paradas, id: number){
-    try{
-        const valores = [p_paradas.nombre, p_paradas.direccion, p_paradas.latitud, p_paradas.longitud, id];
-        const consulta = `UPDATE paradas SET nombre = $1, direccion = $2, latitud = $3, longitud = $4 WHERE id_parada = $5`; 
-        const resultado = await pool.query(consulta, valores);
-
-        if(!resultado.rows[0]){
-            throw new NotFoundError(`La parada con ID ${id} no fue encontrado.`)
-        }
-        return resultado.rows[0];
-    }catch(error){
+        return res.rows[0];
+    } catch (error) {
         errorThrower(error);
     }
 }
 
-export async function eliminarParada(id : number) {
-    try{
-        const valor = [id];
-        const resultado = await pool.query("DELETE FROM paradas WHERE id_parada = $1", valor);
+export async function agregarParada(p: Paradas) {
+    try {
+        const values = [p.nombre, p.direccion, p.latitud, p.longitud];
+        const query = "SELECT * FROM sp_paradas_agregar($1, $2, $3, $4)";
+        const res = await pool.query(query, values);
+        return res.rows[0];
+    } catch (error) {
+        errorThrower(error);
+    }
+}
 
-        if(resultado.rowCount === 0){
-            throw new NotFoundError(`La parada con ID ${id} no fue encontrado.`)
+export async function editarParadaById(id: number, p: Paradas) {
+    try {
+        const values = [p.nombre, p.direccion, p.latitud, p.longitud, id];
+        const query = "SELECT * FROM sp_paradas_actualizar($1, $2, $3, $4, $5)";
+        const res = await pool.query(query, values);
+
+        if (!res.rows[0]) {
+            throw new NotFoundError(`No se puede editar: La parada con ID ${id} no existe.`);
+        }
+
+        return res.rows[0];
+    } catch (error) {
+        errorThrower(error);
+    }
+}
+
+export async function eliminarParadaById(id: number) {
+    try {
+        const res = await pool.query("SELECT sp_paradas_eliminar($1) AS filas_afectadas", [id]);
+
+        if (res.rows[0].filas_afectadas === 0) {
+            throw new NotFoundError(`No se puede eliminar: La parada con ID ${id} no existe.`);
         }
 
         return true;
-    }catch(error){
+    } catch (error) {
         errorThrower(error);
     }
-
-    
 }
