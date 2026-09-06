b08d3198809a07f2389739084a83d2f2fde056e1
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-05 22:20:54 -0600

    refactor: se ha cambiado el service  de usuarios para que la query mande a llamar el sp

--- backend/src/services/usuario.service.ts
+++ backend/src/services/usuario.service.ts
@@ -8,7 +8,7 @@ import { userRol } from "../enums/userRol";
 
 export async function listarUsuarios() {
     try {
-        const resultado = await pool.query("SELECT * FROM Usuarios");
+        const resultado = await pool.query("SELECT * FROM sp_usuarios_listar()");
         return resultado.rows;
     } catch (error) {
         errorThrower(error);
@@ -17,7 +17,7 @@ export async function listarUsuarios() {
 
 export async function buscarUsuarioById(id: number) {
     try {
-        const res = await pool.query('SELECT * FROM Usuarios WHERE id_usuario = $1', [id]);
+        const res = await pool.query('SELECT * FROM sp_usuarios_buscar_por_id($1)', [id]);
 
         if (!res.rows[0]) {
             throw new NotFoundError(`El usuario con ID ${id} no fue encontrado.`);
@@ -33,7 +33,7 @@ export async function agregarUsuario(u: Usuario) {
     try {
         const hashedPassword = await bcrypt.hash(u.password, 10);
         const values = [u.nombre, u.apellido, u.correo, hashedPassword, u.telefono, u.foto_usuario, u.rol, u.correo_verificado];
-        const query = 'INSERT INTO Usuarios(nombre, apellido, correo, password, telefono, foto_usuario, rol, correo_verificado) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
+        const query = 'SELECT * FROM sp_usuarios_agregar($1, $2, $3, $4, $5, $6, $7, $8)';
         const res = await pool.query(query, values);
         return res.rows[0];
     } catch (error) {
@@ -45,7 +45,7 @@ export async function editarUsuarioById(id: number, u: Usuario) {
     try {
         const hashedPassword = await bcrypt.hash(u.password, 10);
         const values = [u.nombre, u.apellido, u.correo, hashedPassword, u.telefono, u.foto_usuario, u.rol, u.correo_verificado, id];
-        const query = 'UPDATE Usuarios SET nombre = $1, apellido = $2, correo = $3, password = $4, telefono = $5, foto_usuario = $6, rol = $7, correo_verificado = $8 WHERE id_usuario = $9 RETURNING *';
+        const query = 'SELECT * FROM sp_usuarios_actualizar($1, $2, $3, $4, $5, $6, $7, $8, $9)';
         const res = await pool.query(query, values);
 
         if (!res.rows[0]) {
@@ -60,9 +60,9 @@ export async function editarUsuarioById(id: number, u: Usuario) {
 
 export async function eliminarUsuarioById(id: number) {
     try {
-        const res = await pool.query('DELETE FROM Usuarios WHERE id_usuario = $1', [id]);
+        const res = await pool.query('SELECT sp_usuarios_eliminar($1) AS eliminadas', [id]);
 
-        if (res.rowCount === 0) {
+        if (res.rows[0].eliminadas === 0) {
             throw new NotFoundError(`No se puede eliminar: El usuario con ID ${id} no existe.`);
         }
 
@@ -74,7 +74,7 @@ export async function eliminarUsuarioById(id: number) {
 
 export async function login(u: UsuarioLoginDTO) {
     try {
-        const res = await pool.query('SELECT * FROM Usuarios WHERE correo = $1', [u.correo]);
+        const res = await pool.query('SELECT * FROM sp_usuarios_buscar_por_correo($1)', [u.correo]);
         if (res.rowCount === 0) {
             throw new ValidationError("Error al logearse", [{
                 campo: "correo",
@@ -103,12 +103,12 @@ export async function login(u: UsuarioLoginDTO) {
 export async function register(u: UsuarioRegisterDTO) {
     try {
         const hashedPassword = await bcrypt.hash(u.password, 10);
-        const values = [u.nombre, u.apellido, u.correo, hashedPassword, u.telefono, u.foto_usuario, userRol.USUARIO];
-        const query = 'INSERT INTO Usuarios(nombre, apellido, correo, password, telefono, foto_usuario, rol) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+        const values = [u.nombre, u.apellido, u.correo, hashedPassword, u.telefono, u.foto_usuario, userRol.USUARIO, false];
+        const query = 'SELECT * FROM sp_usuarios_agregar($1, $2, $3, $4, $5, $6, $7, $8)';
         const res = await pool.query(query, values);
         return res.rows[0];
     } catch (error) {
-        console.log(error)
+        console.log(error);
         errorThrower(error);
     }
 }
