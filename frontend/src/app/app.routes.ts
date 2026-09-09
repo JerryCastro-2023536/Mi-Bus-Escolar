0f410c933cfe1ab5189af53880d9995854c78510
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-09 08:31:36 -0600

    feat: Se agrego la ruta de choferes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { usuariosConfig } from './models/usuarios.model';
 import { Routes } from '@angular/router';
 import { CrudViewComponent } from './components/crud-view/crud-view';
 import { authInterceptor } from './core/interceptor/auth.interceptor';
+import { choferesCrudConfig } from './models/choferes.model';
 
 export const routes: Routes = [
     {
         path: 'usuarios',
         component: CrudViewComponent,
         data: { config: usuariosConfig }
+    },
+    {
+        path: 'choferes',
+        component: CrudViewComponent,
+        data: { config: choferesCrudConfig }
     },
     { path: '', redirectTo: 'usuarios', pathMatch: 'full' }
 ];
