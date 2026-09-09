824338314a0a0192c9af585ee31fe6a582ded7cb
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-08 22:38:12 -0600

    feat: Se agrego interfaz que permite configurar la vista del crud dependiendo de la ruta

---
+++ frontend/src/app/models/crudDTO.interface.ts
@@ -0,0 +1,34 @@
+export interface CrudConfig {
+    title: string;
+    subtitle: string;
+    apiEndpoint: string;
+    kpis: KpiConfig[];
+    tableColumns: TableColumn[];
+    formFields: FormField[];
+}
+
+export interface TableColumn {
+    key: string;
+    label: string;
+    type: 'text' | 'badge' | 'date';
+}
+
+export interface KpiConfig {
+    title: string;
+    valueKey: string;
+    subtitle?: string;
+    icon?: string;
+}
+
+export interface FormField {
+    key: string;
+    label: string;
+    type: 'text' | 'email' | 'password' | 'number' | 'select' | 'checkbox';
+    required?: boolean;
+    options?: FormSelectOption[];
+}
+
+export interface FormSelectOption {
+    value: string | number;
+    label: string;
+}
