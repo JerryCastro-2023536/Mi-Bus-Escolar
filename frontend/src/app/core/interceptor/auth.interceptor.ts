f078856920c6e127d15fd296d30b038bf01a4237
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-08 22:36:56 -0600

    feat: Se agrego interceptor que permite validar tokens y enviarlos

---
+++ frontend/src/app/core/interceptor/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+    const platformId = inject(PLATFORM_ID);
+    if (!isPlatformBrowser(platformId)) {
+        return next(req);
+    }
+
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        return next(req);
+    }
+
+    const authReq = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+    });
+
+    return next(authReq);
+};
