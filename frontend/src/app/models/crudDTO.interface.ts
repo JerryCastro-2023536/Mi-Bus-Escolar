4ead72fcf6f23ef451d2e22cca32a0174499ba13
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-09 17:21:26 -0600

    feat: se agrego el componente crud generico para los dto

---
+++ frontend/src/app/models/crudDTO.interface.ts
@@ -0,0 +1,35 @@
+export interface CrudConfig {
+    title: string;
+    subtitle: string;
+    apiEndpoint: string;
+    idKey: string;
+    kpis: KpiConfig[];
+    tableColumns: TableColumn[];
+    formFields: FormField[];
+}
+
+export interface TableColumn {
+    key: string;
+    label: string;
+    type: 'text' | 'badge' | 'date' | 'number';
+}
+
+export interface KpiConfig {
+    title: string;
+    valueKey: string;
+    subtitle?: string;
+    icon?: string;
+}
+
+export interface FormField {
+    key: string;
+    label: string;
+    type: 'text' | 'email' | 'password' | 'number' | 'select' | 'checkbox';
+    required?: boolean;
+    options?: FormSelectOption[];
+}
+
+export interface FormSelectOption {
+    value: string | number;
+    label: string;
+}
