cf3b8c9965fed119b3ac0ea915b5d3b01f78fce1
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-08 22:35:06 -0600

    feat: Se agrego la ruta CRUD de usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { usuariosConfig } from './models/usuarios.model';
 import { Routes } from '@angular/router';
+import { CrudViewComponent } from './components/crud-view/crud-view';
+import { authInterceptor } from './core/interceptor/auth.interceptor';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'usuarios',
+        component: CrudViewComponent,
+        data: { config: usuariosConfig }
+    },
+    { path: '', redirectTo: 'usuarios', pathMatch: 'full' }
+];
