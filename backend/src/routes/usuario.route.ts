9c0bb5be181d692012878a89306a30d8f5431009
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-13 22:10:09 -0600

    feat: Se añadio la ruta para validar contraseña

--- backend/src/routes/usuario.route.ts
+++ backend/src/routes/usuario.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { getUsuarios, getUsuarioById, postUsuario, putUsuarioByID, deleteUsuarioById } from "../controllers/usuario.controller";
+import { getUsuarios, getUsuarioById, postUsuario, putUsuarioByID, deleteUsuarioById, putUserPassword } from "../controllers/usuario.controller";
 import { validateSchema } from "../utils/middleware/schemaValidator";
-import { createUserSchema, updateUserSchema } from "../validators/user.validator";
+import { cambiarPasswordSchema, createUserSchema, updateUserSchema } from "../validators/user.validator";
 const router = Router();
 
 router.get("/usuarios", getUsuarios);
 router.get("/usuarios/:id", getUsuarioById);
 router.post("/usuarios", validateSchema(createUserSchema), postUsuario);
 router.put("/usuarios/:id", validateSchema(updateUserSchema), putUsuarioByID);
+router.put("/usuarios/password/:id", validateSchema(cambiarPasswordSchema),putUserPassword);
 router.delete("/usuarios/:id", deleteUsuarioById);
 
 export default router;
