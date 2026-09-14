fc248193786b9a735250a15fbcdb373df37c6d20
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-14 17:48:35 -0600

    feat: se han agregado el guard para verificar el token y asi redirigir a distintas rutas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { DashboardComponent } from './dashboard-layout/dashboard-layout';
+import { authGuard, guestGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login, canActivate: [guestGuard] },
+
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
   { path: '**', redirectTo: 'login' }
 ];
