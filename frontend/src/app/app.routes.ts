a8e4f75be0bc8ef68cfa97d9fc770b0c5d2a86d5
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-09-13 20:38:13 -0600

    feat: Se agregaron las primeras rutas de las vistas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { ChoferViewComponents } from './components/chofer-view/chofer-view.components';
+import { AdminUbicacionComponent } from './components/admin-ubicacion/admin-ubicacion.component';
+import { ChoferDashboardComponents } from './components/chofer-dashboard/chofer-dashboard.components';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "mapa", 
+        component: MapaComponent
+    },
+    {
+        path: "chofer",
+        component: AdminUbicacionComponent
+    },
+    {
+        path: "dashboard-chofer",
+        component: ChoferDashboardComponents
+    }
+];
