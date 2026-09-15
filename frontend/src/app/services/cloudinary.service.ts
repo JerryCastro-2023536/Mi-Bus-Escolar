8eaf4f82b63b1b46e5dd7907dc2fcb8387644214
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-14 19:21:01 -0600

    feat: Se añadio servicio para subir imagenes

---
+++ frontend/src/app/services/cloudinary.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { CloudinaryUploadResponse } from '../models/CloudinaryUploadResponse';
+
+@Injectable({ providedIn: 'root' })
+export class CloudinaryService {
+    private http = inject(HttpClient);
+
+    private readonly cloudName = environment.CLOUD_NAME;
+    private readonly uploadPreset = environment.CLOUD_PRESET;
+    private readonly uploadUrl = `https://api.cloudinary.com/v1_1/${this.cloudName}/image/upload`;
+
+    upload(file: File, folder?: string): Observable<string> {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('upload_preset', this.uploadPreset);
+
+        if (folder) {
+            formData.append('folder', folder);
+        }
+
+        return this.http
+            .post<CloudinaryUploadResponse>(this.uploadUrl, formData)
+            .pipe(map(res => res.secure_url));
+    }
+}
