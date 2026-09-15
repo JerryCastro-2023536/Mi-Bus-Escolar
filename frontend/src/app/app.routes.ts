9c1689eb0bf25c74832cf30f9cca061187f366aa
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-14 23:21:47 -0600

    feat: se ha agregado el component del admin view en app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { DashboardComponent } from './components/dashboard-layout/dashboard-layout';
+import { AdminView } from './components/admin-view/admin-view';
 import { authGuard, guestGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login, canActivate: [guestGuard] },
 
-  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: 'admin-view', component: AdminView, canActivate: [authGuard] },
+  { path: 'admin', component: AdminView, canActivate: [authGuard] },
+
+  { path: 'dashboard', component: AdminView, canActivate: [authGuard] },
+  { path: 'dashboard-layout', component: DashboardComponent, canActivate: [authGuard] },
 
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
