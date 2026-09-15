3752ccfdcdfdd2f20c6602900daddb6434998cca
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-14 20:18:17 -0600

    feat: Se cambio el service para que responda una respuesta diferente

--- frontend/src/app/services/crud.service.ts
+++ frontend/src/app/services/crud.service.ts
@@ -1,33 +1,31 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { ApiResponse } from '../models/apiResponseDTO.interface';
-import { environment } from '../../environments/environment';
+import { inject, Injectable } from "@angular/core";
+import { ApiResponse } from "../models/apiResponseDTO.interface";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({ providedIn: 'root' })
 export class CrudService {
     private http = inject(HttpClient);
     private baseUrl = environment.API_URL;
 
-    getAll<T>(endpoint: string): Observable<T[]> {
-        return this.http.get<ApiResponse<T[]>>(`${this.baseUrl}${endpoint}`).pipe(map(res => res.data));
+    getAll<T>(endpoint: string): Observable<ApiResponse<T[]>> {
+        return this.http.get<ApiResponse<T[]>>(`${this.baseUrl}${endpoint}`);
     }
 
-    getById<T>(endpoint: string, id: string | number): Observable<T> {
-        return this.http.get<ApiResponse<T>>(`${this.baseUrl}${endpoint}/${id}`).pipe(map(res => res.data));
+    getById<T>(endpoint: string, id: string | number): Observable<ApiResponse<T>> {
+        return this.http.get<ApiResponse<T>>(`${this.baseUrl}${endpoint}/${id}`);
     }
 
-    create<T>(endpoint: string, data: Partial<T>): Observable<T> {
-        return this.http.post<ApiResponse<T>>(`${this.baseUrl}${endpoint}`, data).pipe(map(res => res.data));
+    create<T>(endpoint: string, data: Partial<T>): Observable<ApiResponse<T>> {
+        return this.http.post<ApiResponse<T>>(`${this.baseUrl}${endpoint}`, data);
     }
 
-    update<T>(endpoint: string, id: string | number, data: Partial<T>): Observable<T> {
-        return this.http.put<ApiResponse<T>>(`${this.baseUrl}${endpoint}/${id}`, data).pipe(map(res => res.data));
+    update<T>(endpoint: string, id: string | number, data: Partial<T>): Observable<ApiResponse<T>> {
+        return this.http.put<ApiResponse<T>>(`${this.baseUrl}${endpoint}/${id}`, data);
     }
 
-    delete(endpoint: string, id: string | number): Observable<void> {
-        return this.http.delete<ApiResponse<void>>(`${this.baseUrl}${endpoint}/${id}`).pipe(map(() => undefined));
+    delete(endpoint: string, id: string | number): Observable<ApiResponse<void>> {
+        return this.http.delete<ApiResponse<void>>(`${this.baseUrl}${endpoint}/${id}`);
     }
-
-    
 }
