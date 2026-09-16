0e40b4410eb928f5eead4ba554e5477947637d50
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-09-16 09:07:39 -0600

    feat: Se agregaron las apis de trazado al global

--- backend/src/routes/apiRouter.ts
+++ backend/src/routes/apiRouter.ts
@@ -19,11 +19,13 @@ import asignacionesRoutes from './asignaciones_rutas.routes';
 import notificacionesRoutes from './notificaciones.routes';
 import pagosRoutes from './pagos.routes';
 import paradasRoutes from './paradas.routes'
+import trazadoRutaRoutes from './trazadoruta.routes'
 
 const apiRouter = Router();
 
 apiRouter.use(anonymRoutes)
 apiRouter.use(verificarAutenticacion)
+apiRouter.use(trazadoRutaRoutes)
 
 apiRouter.use(usuarioRoutes);
 apiRouter.use(viajesRoutes);
@@ -46,5 +48,6 @@ apiRouter.use(asignacionesRoutes);
 apiRouter.use(notificacionesRoutes);
 apiRouter.use(pagosRoutes);
 apiRouter.use(paradasRoutes);
+
 
 export default apiRouter;
