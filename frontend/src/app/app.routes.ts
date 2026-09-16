26016f3bb7cc66c7fa2c03105179c5880eb49b5d
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-15 18:06:22 -0600

    feat: se ha agregado el component de notificaciones en app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { DashboardComponent } from './components/dashboard-layout/dashboard-layout';
 import { AdminView } from './components/admin-view/admin-view';
+import { Notificaciones } from './components/notificaciones/notificaciones';
 import { authGuard, guestGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -13,7 +14,10 @@ export const routes: Routes = [
   { path: 'dashboard', component: AdminView, canActivate: [authGuard] },
   { path: 'dashboard-layout', component: DashboardComponent, canActivate: [authGuard] },
 
+  { path: 'notificaciones', component: Notificaciones, canActivate: [authGuard] },
+
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 
   { path: '**', redirectTo: 'login' }
 ];
+
