05f72ddcd028d9aeaf61f3a78358d6df7b0c2a4b
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-09-16 09:08:29 -0600

    refactor: Se quito las rutas de los componentes de prueba

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,11 @@
 import { Routes } from '@angular/router';
 import { MapaComponent } from './components/mapa/mapa.component';
-import { ChoferViewComponents } from './components/chofer-view/chofer-view.components';
-import { AdminUbicacionComponent } from './components/admin-ubicacion/admin-ubicacion.component';
 import { ChoferDashboardComponents } from './components/chofer-dashboard/chofer-dashboard.components';
 
 export const routes: Routes = [
     {
         path: "mapa", 
         component: MapaComponent
-    },
-    {
-        path: "chofer",
-        component: AdminUbicacionComponent
     },
     {
         path: "dashboard-chofer",
