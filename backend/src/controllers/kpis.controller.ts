b78437a3289e2f4e25920facc2c9b486a89172b3
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-16 10:31:36 -0600

    feat: Se agregaron todos los controladores kpi

--- backend/src/controllers/kpis.controller.ts
+++ backend/src/controllers/kpis.controller.ts
@@ -1,16 +1,55 @@
-import { obtenerKpisUsuarios } from "../services/kpis.service";
 import { Request, Response, NextFunction } from "express";
+import {
+    obtenerKpisUsuarios,
+    obtenerKpisAsignacionesRuta,
+    obtenerKpisAsistencias,
+    obtenerKpisChoferes,
+    obtenerKpisColegios,
+    obtenerKpisEstudiantes,
+    obtenerKpisIncidencias,
+    obtenerKpisNotificaciones,
+    obtenerKpisPagos,
+    obtenerKpisParadas,
+    obtenerKpisProveedores,
+    obtenerKpisRutaParada,
+    obtenerKpisRutas,
+    obtenerKpisServicios,
+    obtenerKpisUbicacionesBus,
+    obtenerKpisValoraciones,
+    obtenerKpisVehiculos,
+    obtenerKpisViajes
+} from "../services/kpis.service";
 
-
-export async function getUsuariosKpis(_req: Request, res: Response, next: NextFunction) {
-    try {
-        const kpis = await obtenerKpisUsuarios();
-        return res.status(200).json({
-            success: true,
-            message: "KPIs cargados correctamente",
-            data: kpis
-        });
-    } catch (error) {
-        next(error);
-    }
+function responderKpis(fn: () => Promise<any>) {
+    return async (_req: Request, res: Response, next: NextFunction) => {
+        try {
+            const kpis = await fn();
+            return res.status(200).json({
+                success: true,
+                message: "KPIs cargados correctamente",
+                data: kpis
+            });
+        } catch (error) {
+            next(error);
+        }
+    };
 }
+
+export const getKpisUsuarios = responderKpis(obtenerKpisUsuarios);
+export const getKpisAsignacionesRuta = responderKpis(obtenerKpisAsignacionesRuta);
+export const getKpisAsistencias = responderKpis(obtenerKpisAsistencias);
+export const getKpisChoferes = responderKpis(obtenerKpisChoferes);
+export const getKpisColegios = responderKpis(obtenerKpisColegios);
+export const getKpisEstudiantes = responderKpis(obtenerKpisEstudiantes);
+export const getKpisIncidencias = responderKpis(obtenerKpisIncidencias);
+export const getKpisNotificaciones = responderKpis(obtenerKpisNotificaciones);
+export const getKpisPagos = responderKpis(obtenerKpisPagos);
+export const getKpisParadas = responderKpis(obtenerKpisParadas);
+export const getKpisProveedores = responderKpis(obtenerKpisProveedores);
+export const getKpisRutaParada = responderKpis(obtenerKpisRutaParada);
+export const getKpisRutas = responderKpis(obtenerKpisRutas);
+export const getKpisServicios = responderKpis(obtenerKpisServicios);
+export const getKpisUbicacionesBus = responderKpis(obtenerKpisUbicacionesBus);
+export const getKpisValoraciones = responderKpis(obtenerKpisValoraciones);
+export const getKpisVehiculos = responderKpis(obtenerKpisVehiculos);
+export const getKpisViajes = responderKpis(obtenerKpisViajes);
