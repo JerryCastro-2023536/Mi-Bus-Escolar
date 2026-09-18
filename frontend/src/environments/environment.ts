0a875a5f6064c9844eb3a0cbf565b4571882869a
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-17 20:07:22 -0600

    remove: Se eliminaron archivos de más

--- frontend/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    API_URL: "http://localhost:3000/api",
-    CLOUD_NAME: "l8hnnzud",
-    CLOUD_PRESET: "Mi_bus_escolar_preset",
-    CLOUDINARY_API_KEY: "822413987345274",
-    CLOUDINARY_API_SECRET: "lB45IrViINP5MxMgdYQOcHmF7K8"
-};
