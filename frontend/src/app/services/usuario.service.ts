23a4dba8c137010f743bef81454619a921f05fe1
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-17 20:08:44 -0600

    refactor: Se agrego servicio de usuario

--- frontend/src/app/services/usuario.service.ts
+++ frontend/src/app/services/usuario.service.ts
@@ -1,30 +1,31 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CrudService } from './crud.service';
-import { Usuario } from '../../../../backend/src/models/usuario';
+import { ApiResponse } from '../models/apiResponseDTO.interface';
+import { UsuarioDTO } from '../models/usuarioDTO.interface';
 
 @Injectable({ providedIn: 'root' })
 export class UsuarioService {
   private crud = inject(CrudService);
   private endpoint = '/usuarios';
 
-  listarUsuarios(): Observable<Usuario[]> {
-    return this.crud.getAll<Usuario>(this.endpoint);
+  listarUsuarios(): Observable<ApiResponse<UsuarioDTO[]>> {
+    return this.crud.getAll<UsuarioDTO>(this.endpoint);
   }
 
-  buscarUsuarioById(id: number): Observable<Usuario> {
-    return this.crud.getById<Usuario>(this.endpoint, id);
+  buscarUsuarioById(id: number): Observable<ApiResponse<UsuarioDTO>> {
+    return this.crud.getById<UsuarioDTO>(this.endpoint, id);
   }
 
-  agregarUsuario(data: Partial<Usuario>): Observable<Usuario> {
-    return this.crud.create<Usuario>(this.endpoint, data);
+  agregarUsuario(data: Partial<UsuarioDTO>): Observable<ApiResponse<UsuarioDTO>> {
+    return this.crud.create<UsuarioDTO>(this.endpoint, data);
   }
 
-  editarUsuarioById(id: number, data: Partial<Usuario>): Observable<Usuario> {
-    return this.crud.update<Usuario>(this.endpoint, id, data);
+  editarUsuarioById(id: number, data: Partial<UsuarioDTO>): Observable<ApiResponse<UsuarioDTO>> {
+    return this.crud.update<UsuarioDTO>(this.endpoint, id, data);
   }
 
-  eliminarUsuarioById(id: number): Observable<void> {
+  eliminarUsuarioById(id: number): Observable<ApiResponse<void>> {
     return this.crud.delete(this.endpoint, id);
   }
 }
