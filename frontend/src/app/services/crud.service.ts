b1c2f9b4e2e6d8911922672f54068e5890b53737
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-19 00:45:44 -0600

    refactor: se ha cambiado el nombre de la ruta de enviroment

--- frontend/src/app/services/crud.service.ts
+++ frontend/src/app/services/crud.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from "@angular/core";
 import { ApiResponse } from "../models/apiResponseDTO.interface";
 import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
-import { environment } from "../../environments/environment";
+import { environment } from "../../environments/enviroment";
 
 @Injectable({ providedIn: 'root' })
 export class CrudService {
