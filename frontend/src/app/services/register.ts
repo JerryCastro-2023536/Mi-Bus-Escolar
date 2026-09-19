579481a9ad8d2b3197fdb0cadb62c5d3e7b0cb47
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-09-18 22:24:50 -0600

    refactor: se eliminaron/cambiaron importaciones innecesarias

--- frontend/src/app/services/register.ts
+++ frontend/src/app/services/register.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthResponse } from './login';
-import { UsuarioRegisterDTO } from '../../../../backend/src/models/usuario';
+import { UsuarioRegisterDTO } from '../models/usuarioDTO.interface';
 import { environment } from '../../environments/enviroment';
 
 @Injectable({
