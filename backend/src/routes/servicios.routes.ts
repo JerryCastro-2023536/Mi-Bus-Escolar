4985b79f73e557a133cd896d6a6c0a4b76cbbcc4
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-19 20:25:35 -0600

    feat: Se añadieron rutas para administrar servicios

--- backend/src/routes/servicios.routes.ts
+++ backend/src/routes/servicios.routes.ts
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { obtenerServicios, obtenerServicioPorId, crearServicio, editarServicio, eliminarServicios } from "../controllers/servicios.controller"
+import { obtenerServicios, obtenerServicioPorId, crearServicio, editarServicio, eliminarServicios, getMisServicios, postMiServicio, putMiServicio, deleteMiServicio } from "../controllers/servicios.controller"
 import { validateSchema } from "../utils/middleware/schemaValidator";
 import { createServicioSchema, updateServicioSchema } from "../validators/servicios.validator";
 export const router = Router();
@@ -9,5 +9,10 @@ router.get("/servicios/:id", obtenerServicioPorId);
 router.post("/servicios", validateSchema(createServicioSchema),crearServicio);
 router.put("/servicios/:id", validateSchema(updateServicioSchema),editarServicio);
 router.delete("/servicios/:id", eliminarServicios);
+
+router.get("/servicios/proveedor/:idUsuario", getMisServicios);
+router.post("/servicios/proveedor/:idUsuario", postMiServicio);
+router.put("/servicios/proveedor/:idUsuario/:idServicio", putMiServicio);
+router.delete("/servicios/proveedor/:idUsuario/:idServicio", deleteMiServicio);
 
 export default router;
