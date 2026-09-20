36440db97ae9b787bed7ba79b51ad9452f169c17
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-19 22:55:00 -0600

    feat: Se agregaron rutas para vehiculos-proveedor

--- backend/src/routes/vehiculos.routes.ts
+++ backend/src/routes/vehiculos.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { obtenerVehiculos, obtenerVehiculoPorId, crearVehiculo, editarVehiculo, eliminarVehiculos } from "../controllers/vehiculos.controller"
+import { obtenerVehiculos, obtenerVehiculoPorId, crearVehiculo, editarVehiculo, eliminarVehiculos, getMisVehiculos, postMiVehiculo, putMiVehiculo, deleteMiVehiculo } from "../controllers/vehiculos.controller"
 import { validateSchema } from "../utils/middleware/schemaValidator";
 import { createVehiculoSchema, updateVehiculoSchema } from "../validators/vehiculos.validator";
 export const router = Router();
@@ -9,5 +9,10 @@ router.get("/vehiculos/:id", obtenerVehiculoPorId);
 router.post("/vehiculos", validateSchema(createVehiculoSchema),crearVehiculo);
 router.put("/vehiculos/:id", validateSchema(updateVehiculoSchema),editarVehiculo);
 router.delete("/vehiculos/:id", eliminarVehiculos);
+
+router.get("/vehiculos/proveedor/:idUsuario", getMisVehiculos);
+router.post("/vehiculos/proveedor/:idUsuario", postMiVehiculo);
+router.put("/vehiculos/proveedor/:idUsuario/:idVehiculo", putMiVehiculo);
+router.delete("/vehiculos/proveedor/:idUsuario/:idVehiculo", deleteMiVehiculo);
 
 export default router;
