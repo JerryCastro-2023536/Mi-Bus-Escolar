05d53486f9ce502db872abb06a4c41a0910e7115
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-09-21 11:55:04 -0600

    feat: se agregaron rutas para obtener proveedores, rutas, servicios y valoraciones en la landing

--- backend/src/routes/anonym.route.ts
+++ backend/src/routes/anonym.route.ts
@@ -2,10 +2,15 @@ import { Router } from 'express';
 import { loginUsuario, registerUsuario } from '../controllers/usuario.controller';
 import { validateSchema } from '../utils/middleware/schemaValidator';
 import { loginUserSchema, registerUserSchema } from '../validators/user.validator';
+import { getValoraciones1, obtenerProveedores1, obtenerRutas1, obtenerServicios1 } from '../controllers/valoraciones-landing.controller';
 
 const router = Router();
 
 router.post('/login', validateSchema(loginUserSchema),loginUsuario);
 router.post('/register', validateSchema(registerUserSchema),registerUsuario);
+router.get('/landing/proveedores', obtenerProveedores1);
+router.get('/landing/rutas', obtenerRutas1);
+router.get('/landing/servicios', obtenerServicios1);
+router.get('/landing/valoraciones', getValoraciones1);
 
 export default router;
