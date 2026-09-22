dcd39842014adc4306fc8562a8cd427feb551554
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-09-21 21:05:53 -0600

    feat: Se agrego el endopoint de las rutas

--- backend/src/routes/choferDashboard.routes.ts
+++ backend/src/routes/choferDashboard.routes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import {
   getVehiculosPorChofer,
   getEstudiantesPorChofer,
-  getReportesPorChofer
+  getReportesPorChofer,
+  getRutasPorChofer
 } from "../controllers/choferDashboard.controller";
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.get("/chofer-dashboard/:id/vehiculos", getVehiculosPorChofer);
 router.get("/chofer-dashboard/:id/estudiantes", getEstudiantesPorChofer);
 router.get("/chofer-dashboard/:id/reportes", getReportesPorChofer);
+router.get("/chofer-dashboard/:id/rutas", getRutasPorChofer);
 
 export default router;
