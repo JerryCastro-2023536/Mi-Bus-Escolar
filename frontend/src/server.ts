dc73bda5441091ac8cb91cabce6f26da87eee4f5
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-23 07:18:19 -0600

    fix: Se corrigio subir foto deploy

--- frontend/src/server.ts
+++ frontend/src/server.ts
@@ -4,14 +4,21 @@ import {
   isMainModule,
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
+
 import express from 'express';
 import { join } from 'node:path';
 
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
 
+const angularApp = new AngularNodeAppEngine({
+  trustProxyHeaders: [
+    'x-forwarded-proto',
+    'x-forwarded-host',
+    'x-forwarded-for'
+  ]
+});
 
 app.use(
   express.static(browserDistFolder, {
@@ -25,19 +32,22 @@ app.use((req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response
+        ? writeResponseToNodeResponse(response, res)
+        : next(),
     )
     .catch(next);
 });
 
 if (isMainModule(import.meta.url) || process.env['pm_id']) {
   const port = process.env['PORT'] || 4000;
+
   app.listen(port, (error) => {
     if (error) {
       throw error;
     }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`Node Express server listening on port ${port}`);
   });
 }
 
