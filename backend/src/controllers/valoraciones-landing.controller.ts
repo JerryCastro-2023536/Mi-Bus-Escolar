d9423921d155a854100ab153b38e275547642491
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-22 19:30:34 -0600

    feat: Se adapto a la nueva DB

--- backend/src/controllers/valoraciones-landing.controller.ts
+++ backend/src/controllers/valoraciones-landing.controller.ts
@@ -2,17 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { listarProveedores } from "../services/proveedores.service";
 import { listarRutas } from "../services/rutas.service";
 import { listarServicios } from "../services/servicios.service";
-import { listarValoraciones } from "../services/valoraciones.service";
-import { errorThrower } from "../utils/middleware/errorThrower";
-import { pool } from "../config/conexion";
+import { listarValoraciones, listarValoracionesDetalle } from "../services/valoraciones.service";
 
 export async function obtenerProveedores1(_req: Request, res: Response, next: NextFunction) {
     try {
-        const proveedores = await listarProveedores();
         return res.status(200).json({
             success: true,
             message: "Proveedores cargados correctamente",
-            data: proveedores
+            data: await listarProveedores(),
         });
     } catch (error) {
         next(error);
@@ -21,11 +18,10 @@ export async function obtenerProveedores1(_req: Request, res: Response, next: Ne
 
 export async function obtenerRutas1(_req: Request, res: Response, next: NextFunction) {
     try {
-        const rutas = await listarRutas();
         return res.status(200).json({
             success: true,
             message: "Rutas cargadas correctamente",
-            data: rutas
+            data: await listarRutas(),
         });
     } catch (error) {
         next(error);
@@ -34,26 +30,40 @@ export async function obtenerRutas1(_req: Request, res: Response, next: NextFunc
 
 export async function obtenerServicios1(_req: Request, res: Response, next: NextFunction) {
     try {
-        const servicios = await listarServicios();
         return res.status(200).json({
             success: true,
             message: "Servicios cargados correctamente",
-            data: servicios
+            data: await listarServicios(),
         });
     } catch (error) {
         next(error);
     }
 }
 
-export async function getValoraciones1(req: Request, res: Response, next: NextFunction){
-    try{
-        const datos = await listarValoraciones();
+export async function getValoraciones1(_req: Request, res: Response, next: NextFunction) {
+    try {
         return res.status(200).json({
             success: true,
             message: "Valoraciones cargadas",
-            data: datos
+            data: await listarValoraciones(),
         });
-    }catch(error){
-        next(error)
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function getValoracionesDetalleLanding(
+    _req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    try {
+        return res.status(200).json({
+            success: true,
+            message: "Valoraciones detalladas cargadas",
+            data: await listarValoracionesDetalle(),
+        });
+    } catch (error) {
+        next(error);
     }
 }
