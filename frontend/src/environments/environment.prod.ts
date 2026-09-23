f781ba4542bdd3d1b854668da7172c913b704683
Gabrielcalderon-2022327 <gcalderon-2022327@kinal.edu.gt>
2026-09-22 22:30:18 -0600

    feat: Actualización credenciales deploy

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    API_URL: "http://localhost:3000/api",
+    production: true,
+    API_URL: 'https://mi-bus-escolar.onrender.com/api',
     CLOUD_NAME: "l8hnnzud",
     CLOUD_PRESET: "Mi_bus_escolar_preset",
 };
